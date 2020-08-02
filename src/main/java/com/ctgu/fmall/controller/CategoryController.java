07e4409d5b9424d22bca0f840b834d0872a90057
triwhiter <245886461@qq.com>
2020-08-02 16:27:30 +0800

    返回目录接口

--- src/main/java/com/ctgu/fmall/controller/CategoryController.java
+++ src/main/java/com/ctgu/fmall/controller/CategoryController.java
@@ -1,9 +1,16 @@
 package com.ctgu.fmall.controller;
 
 
+import com.ctgu.fmall.entity.Category;
+import com.ctgu.fmall.service.CategoryService;
+import com.ctgu.fmall.utils.ResultUtil;
+import com.ctgu.fmall.vo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +23,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @RequestMapping("/all")
+    private Result getAllCategory(){
+        List<Category> list= categoryService.list(null);
+        return ResultUtil.success(list);
+    }
 
 }
 
