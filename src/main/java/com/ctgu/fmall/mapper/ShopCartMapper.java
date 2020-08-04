2af65a2ea89b9da6056c8e78df65ce81fc97ce44
FlxLin <478237430@qq.com>
2020-08-04 11:40:02 +0800

    完成数据处理

--- src/main/java/com/ctgu/fmall/mapper/ShopCartMapper.java
+++ src/main/java/com/ctgu/fmall/mapper/ShopCartMapper.java
@@ -20,7 +20,7 @@ import java.util.Map;
 @Repository
 public interface ShopCartMapper extends BaseMapper<ShopCart> {
 
-    @Select("select ANY_VALUE(product.store) as store,ANY_VALUE(product_image.img_url) as img_url, ANY_VALUE(product.name) as name,ANY_VALUE(product.promote_price) as promote_price,ANY_VALUE(product.original_price) as original_price,ANY_VALUE(product.id) as pid,ANY_VALUE(shopcart_num) as shopcart_num" +
+    @Select("select ANY_VALUE(product.store) as store,ANY_VALUE(product_image.img_url) as img_url, ANY_VALUE(product.name) as name,format(ANY_VALUE(product.promote_price),2) as promote_price,FORMAT(ANY_VALUE(product.original_price),2) as original_price,ANY_VALUE(product.id) as pid,ANY_VALUE(shopcart_num) as shopcart_num" +
             " from product, product_image, shop_cart where " +
             "shop_cart.uid = #{uid} and product.id = shop_cart.pid and product.id = product_image.pid group by product_image.pid;")
     List<Map> getShopCartInfo(int uid);
