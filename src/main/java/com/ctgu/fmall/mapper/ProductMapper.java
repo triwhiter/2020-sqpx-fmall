d4e0dd52edaa676cabb885238e06e8637ef0662a
FlxLin <478237430@qq.com>
2020-08-06 18:21:24 +0800

    完成获取逻辑删除的商品

--- src/main/java/com/ctgu/fmall/mapper/ProductMapper.java
+++ src/main/java/com/ctgu/fmall/mapper/ProductMapper.java
@@ -3,7 +3,11 @@ package com.ctgu.fmall.mapper;
 import com.ctgu.fmall.entity.Product;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -18,4 +22,7 @@ public interface ProductMapper extends BaseMapper<Product> {
 
     @Delete("delete from product where id = #{id};")
     int delProduct(int id);
+
+    @Select("select * from product where deleted = 1")
+    List<Map> getRemoveProduct();
 }
