9212718de95e69a16a9b86a803fbc7f77e371c13
FlxLin <478237430@qq.com>
2020-08-06 16:03:15 +0800

    完成商品ç删除

--- src/main/java/com/ctgu/fmall/service/ProductService.java
+++ src/main/java/com/ctgu/fmall/service/ProductService.java
@@ -2,6 +2,7 @@ package com.ctgu.fmall.service;
 
 import com.ctgu.fmall.entity.Product;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.ctgu.fmall.vo.Result;
 
 /**
  * <p>
@@ -13,4 +14,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface ProductService extends IService<Product> {
 
+    Result delProduct(int id);
 }
--- src/main/java/com/ctgu/fmall/service/impl/ProductServiceImpl.java
+++ src/main/java/com/ctgu/fmall/service/impl/ProductServiceImpl.java
@@ -1,9 +1,15 @@
 package com.ctgu.fmall.service.impl;
 
-import com.ctgu.fmall.entity.Product;
-import com.ctgu.fmall.mapper.ProductMapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.ctgu.fmall.common.eums.ResultEnum;
+import com.ctgu.fmall.entity.*;
+import com.ctgu.fmall.mapper.*;
 import com.ctgu.fmall.service.ProductService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ctgu.fmall.utils.ResultUtil;
+import com.ctgu.fmall.vo.Result;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +23,46 @@ import org.springframework.stereotype.Service;
 @Service
 public class ProductServiceImpl extends ServiceImpl<ProductMapper, Product> implements ProductService {
 
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Autowired
+    private ProductImageMapper productImageMapper;
+
+    @Autowired
+    private OrderDetailMapper orderDetailMapper;
+
+    @Autowired
+    private ShopCartMapper shopCartMapper;
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+    @Override
+    public Result delProduct(int id) {
+        QueryWrapper<ProductImage> productImageQueryWrapper = new QueryWrapper<>();
+        productImageQueryWrapper.eq("pid",id);
+        int deleteProductImage = productImageMapper.delete(productImageQueryWrapper);
+
+        QueryWrapper<Comment> commentQueryWrapper = new QueryWrapper<>();
+        commentQueryWrapper.eq("pid",id);
+        int deleteComment = commentMapper.delete(commentQueryWrapper);
+
+        QueryWrapper<ShopCart> shopCartQueryWrapper = new QueryWrapper<>();
+        shopCartQueryWrapper.eq("pid",id);
+        int deleteShopCart = shopCartMapper.delete(shopCartQueryWrapper);
+
+        QueryWrapper<OrderDetail> orderDetailQueryWrapper = new QueryWrapper<>();
+        orderDetailQueryWrapper.eq("pid",id);
+        int deleteOrderDetailMapper = orderDetailMapper.delete(orderDetailQueryWrapper);
+
+        int delProduct = productMapper.delProduct(id);
+
+        if (delProduct != 0){
+            return ResultUtil.success(ResultEnum.SUCCESS);
+        }else {
+            return ResultUtil.error(ResultEnum.FAIL);
+        }
+
+    }
 }
